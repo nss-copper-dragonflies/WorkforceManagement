243415a8d76fbb47174e27beada7754b6ee80bba
Jordan Rosas <jordanrosas1203@gmail.com>
2019-04-12 09:16:11 -0500

    accepted changes

---
+++ BangazonWorkForce/Models/ViewModel/ComputerCreateViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkForce.Models.ViewModel
+{
+    public class ComputerCreateViewModel
+    {
+        public ComputerCreateViewModel()
+        {
+            Employees = new List<Employee>();
+        }
+
+        public ComputerCreateViewModel(string connectionString)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString)
+            {
+                conn.Open();
+            using (SqlCommand cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = @"Select e.FirstName, e.LastName
+                                        From Employee";
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+            }
+        }
+    }
+}
